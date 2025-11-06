d15362a41cdd8877a178e5a348486b1197640ca2
n4runs <narunavpg@gmail.com>
2025-11-06 12:30:53 +0700

    Update no9.sh

--- no9.sh
+++ no9.sh
@@ -1,22 +1,35 @@
-# Di Pharazon
+# Langkah Konfigurasi dan Uji
+#  Di node Pharazon
+
+# Pastikan DNS diarahkan ke Minastir (agar bisa resolve domain *.K09.com)
 printf "nameserver 10.68.5.2\n" > /etc/resolv.conf
-cat >/etc/apt/apt.conf.d/00proxy <<'EOF'
+
+# Tambahkan proxy agar bisa apt-get lewat Minastir (sebagai internet gateway)
+cat > /etc/apt/apt.conf.d/00proxy <<'EOF'
 Acquire::http::Proxy  "http://10.68.5.2:3128";
 Acquire::https::Proxy "http://10.68.5.2:3128";
 EOF
 
+# Update dan install tools untuk testing
 apt update -o Acquire::ForceIPv4=true -y
 apt install -y lynx curl
 
-# Uji koneksi Laravel API
-lynx -dump http://elendil.k09.com/api/airing
-curl http://elendil.k09.com/api/airing
+# Uji koneksi API Laravel dari setiap worker
 
-lynx -dump http://isildur.k09.com/api/airing
-curl http://isildur.k09.com/api/airing
+# Elendil
+echo "=== Testing Elendil ==="
+lynx -dump http://elendil.K09.com:8001/api/airing
+curl http://elendil.K09.com:8001/api/airing
+echo
 
-lynx -dump http://anarion.k09.com/api/airing
-curl http://anarion.k09.com/api/airing
+# Isildur
+echo "=== Testing Isildur ==="
+lynx -dump http://isildur.K09.com:8002/api/airing
+curl http://isildur.K09.com:8002/api/airing
+echo
 
-
-
+# Anarion
+echo "=== Testing Anarion ==="
+lynx -dump http://anarion.K09.com:8003/api/airing
+curl http://anarion.K09.com:8003/api/airing
+echo
